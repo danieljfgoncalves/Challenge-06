c87b6bfb0b55e277ba1668989fefff4d0ecb0319
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-08 02:18:31 -0400

    Initial Commit

---
+++ Challenge-06/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Challenge-06
+//
+//  Created by Daniel Goncalves on 2015-04-08.
+//  Copyright (c) 2015 Daniel Goncalves. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
